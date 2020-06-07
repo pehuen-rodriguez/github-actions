771ca89b74531330573716dfc82df775830f2053
Pehuén <pehuen.rodriguez@nayra.coop>
2020-06-07 19:10:02 -0300

    Test aim changes not to server folder

---
+++ server/server.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const http = require('http')
+const bodyParser = require('body-parser')
+const pino = require('express-pino-logger')()
+
+const app = express()
+const port = process.env.SERVER_PORT || 3011
+const server = http.createServer(app)
+
+const io = require('./io')
+
+io.init(server)
+
+// const routes = require("./routes");
+// app.use(routes);
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(pino)
+
+// eslint-disable-next-line
+server.listen(port, () => console.log(`Listening on port ${port}`))
